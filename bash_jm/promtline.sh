f61ff5481382d14af378042efba957a6c2a504f7
Jose M. Facil <jmfacil@unizar.es>
2018-09-25 10:14:34 +0200

    time on promt and some vim settings

--- bash_jm/promtline.sh
+++ bash_jm/promtline.sh
@@ -1,2 +1,2 @@
 #Personal promt ORANGE
-PS1='${debian_chroot:+($debian_chroot)}\[\033[1;95;38;5;214m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]> '
+PS1='[\t]${debian_chroot:+($debian_chroot)}\[\033[1;95;38;5;214m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]> '
